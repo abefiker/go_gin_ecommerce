345b44b9bc727fc392e01f2728451d5e736df611
abfiker <new94abemelek@gmail.com>
2024-03-12 22:18:23 +0300

    "models finished and controllers are ready"

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,1 +1,25 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func HashedPassword(password string) string {
+
+}
+func VerfyPassword(userPassword string, givenPassword string) (bool string) {
+
+}
+func SignUp() gin.HandlerFunc{
+
+}
+func Login() gin.HandlerFunc{
+
+}
+func ProductViewAdmin() gin.HandlerFunc {
+
+}
+func SearchProduct() gin.HandlerFunc{
+
+}
+func SearchProductByQuery() gin.HandlerFunc {
+
+}
