bf1ab54689282d8cc505329fd56186073fdd62cf
abfiker <new94abemelek@gmail.com>
2024-03-12 23:35:31 +0300

    "sign up is ongoing

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,6 +1,15 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/abefiker/go_gin_ecommerce/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 func HashedPassword(password string) string {
 
@@ -8,16 +17,51 @@ func HashedPassword(password string) string {
 func VerfyPassword(userPassword string, givenPassword string) (bool string) {
 
 }
-func SignUp() gin.HandlerFunc{
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+			return
+		}
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "user already exist"})
+			return
+		}
+		count, err := UserCollection.CountDocuments(ctx, bson.H{"phone": user.Phone})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "this phone already exist"})
+		}
+	}
 }
-func Login() gin.HandlerFunc{
+func Login() gin.HandlerFunc {
 
 }
 func ProductViewAdmin() gin.HandlerFunc {
 
 }
-func SearchProduct() gin.HandlerFunc{
+func SearchProduct() gin.HandlerFunc {
 
 }
 func SearchProductByQuery() gin.HandlerFunc {
--- database/databasesetup.go
+++ database/databasesetup.go
@@ -30,10 +30,14 @@ func DBSetup() *mongo.Client {
 	return client
 }
 
-func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+var Client *mongo.Client = DBSetup()
 
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return collection
 }
 
 func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
-
+	var productCollection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return productCollection
 }
