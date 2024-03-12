bd16c28772643f21dfc201d04ff28908a5591fae
abfiker <new94abemelek@gmail.com>
2024-03-12 22:49:42 +0300

    "error customization and databse set up"

--- database/databasesetup.go
+++ database/databasesetup.go
@@ -1,1 +1,39 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBSetup() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://go-mongodb:MongoDb@be1994@cluster0.sxvbxay.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Println("failed to connect to mongodb :( ")
+		return nil
+	}
+	fmt.Println("Successfully connected to mongodb")
+	return client
+}
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+
+}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+
+}
